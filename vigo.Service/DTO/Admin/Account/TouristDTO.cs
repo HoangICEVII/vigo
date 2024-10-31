cef6f6a31a1282d9118b83c4264f5c79970a79f4
HoangICEVII <nguyenviethoang2909@gmail.com>
2024-11-01 00:00:05 +0700

    update

--- vigo.Service/DTO/Admin/Account/TouristDTO.cs
+++ vigo.Service/DTO/Admin/Account/TouristDTO.cs
@@ -11,6 +11,8 @@ namespace vigo.Service.DTO.Admin.Account
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Avatar { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
         public DateTime DOB { get; set; }
         public string Gender { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
--- vigo.Service/DTO/Admin/Account/TouristDetailDTO.cs
+++ vigo.Service/DTO/Admin/Account/TouristDetailDTO.cs
@@ -12,6 +12,8 @@ namespace vigo.Service.DTO.Admin.Account
         public string Name { get; set; } = string.Empty;
         public string Avatar { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
         public DateTime DOB { get; set; }
         public string Gender { get; set; } = string.Empty;
         public Guid AccountId { get; set; }
